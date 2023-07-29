cc4f780577f1661576e44721157d7d8a57374f42
Varshini R <varshinivr@gmail.com>
2023-07-29 13:30:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const express=require('express')
+const app=express();
+const path=require('path')
+app.set('view engine','ejs')
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')))
+const bookData=require('./books.json')
+//console.log(bookData)
+
+app.listen(3000,()=>{
+    console.log('listening');
+})
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+app.get('/view',(req,res)=>{
+    res.render('home',{books:bookData}) 
+})
+
+app.get('/view/:subbook',(req,res)=>{
+    const {subbook}=req.params;
+    const data=bookData[subbook];
+    if(data)
+    {
+        console.log(data);
+        res.render('subbook',{data:data})
+    }
+    else
+    {
+        res.render('notfound',{subbook});
+    }
+})
+
+
